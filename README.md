7c91bc53e4e98c0de3c6c747677c0d24a8b856d4
hancy <hancyxhx@gmail.com>
2013-09-01 20:54:41 +0800

    support command argument for reading input from file

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ A C Language Implement of Shuffle Algorithm
 
 done:
 1. support for reading input with very long line
+2. support command argument for reading input from file
 
 
 todo : 
-2. support for reading input from stdin or file
 
-test by wangxx
+
--- src/shuffle.c
+++ src/shuffle.c
@@ -1,52 +1,94 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
-#define DRAW_DECK stdin
 #define MAX_SHUFFLE_DECK_SIZE 1024 * 500
-
 typedef char* Poker;
 
-Poker draw(FILE *draw_deck){
-  char* buf = NULL;
-  int buf_len = 0;
-  int bytes_read;
 
-  bytes_read = getline(&buf, &buf_len, draw_deck);
-  if(bytes_read == -1)
-    return NULL;
-  else{
-    char *poker = malloc(bytes_read + 1);
-    strcpy(poker, buf);
-    return poker;
-  }
+Poker draw(FILE *draw_deck){
+    char* buf = NULL;
+    int buf_len = 0;
+    int bytes_read;
+
+    bytes_read = getline(&buf, &buf_len, draw_deck);
+    if(bytes_read == -1)
+	return NULL;
+    else{
+	char *poker = malloc(bytes_read + 1);
+	strcpy(poker, buf);
+	return poker;
+    }
 }
 
 void show_deck(Poker deck[]){
-  int i;
-  for (i=0; deck[i] != NULL; i++){
-    printf("%d", atoi(deck[i]));
-    printf("%c", (deck[i + 1] != NULL) ? ' ' : '\n');
-  }
+    int i;
+    for (i=0; deck[i] != NULL; i++){
+	printf("%d", atoi(deck[i]));
+	printf("%c", (deck[i + 1] != NULL) ? ' ' : '\n');
+    }
 }
 
-Poker shuffle_deck[MAX_SHUFFLE_DECK_SIZE];
+
+struct {
+    FILE *draw_deck;
+    Poker *shuffle_deck;
+} global_args;
+
+void usage(){
+    printf("Usage: shuffle [-f file]\n");
+    exit(-1);
+}
+
+void parse_arg(int argc, char *argv[]){
+    /* set default value for global argument */
+    global_args.draw_deck = stdin;
+    global_args.shuffle_deck = malloc(sizeof(Poker) * MAX_SHUFFLE_DECK_SIZE);
+
+    /* read argument */
+    int ch;
+    while ((ch = getopt(argc, argv, "f:h")) != -1){
+	switch (ch) {
+	case 'f':
+	    if ((global_args.draw_deck = fopen(optarg, "r")) == NULL){
+		fprintf(stderr, "EROOR: open %s\n", optarg);
+		exit(-1);
+	    }
+	    break;
+	case 'h':
+	    usage();
+	    break;
+	case '?':
+	defualt:
+	    usage();
+	    break;
+	}
+    }
+
+    return;
+}
+
+
+
+
 int main (int argc, char *argv[] ){
-  srand( (unsigned int)time(NULL) );
+    parse_arg(argc, argv);
 
-  int pokercnt=0;
-  Poker crrnt_poker;
-  while ( (crrnt_poker = draw(DRAW_DECK)) != NULL){
-    pokercnt++;
+    srand( (unsigned int)time(NULL) );
+    int pokercnt=0;
+    Poker crrnt_poker;
+    while ( (crrnt_poker = draw(global_args.draw_deck)) != NULL){
+    	pokercnt++;
 
-    int insert_position = rand() % pokercnt;
-    if (insert_position != pokercnt -1)
-      shuffle_deck[pokercnt - 1] = shuffle_deck[insert_position];
-    shuffle_deck[insert_position] = crrnt_poker;
-  }
+    	int insert_position = rand() % pokercnt;
+    	if (insert_position != pokercnt -1)
+    	    global_args.shuffle_deck[pokercnt - 1] = global_args.shuffle_deck[insert_position];
+    	global_args.shuffle_deck[insert_position] = crrnt_poker;
+    }
 
-  show_deck(shuffle_deck);
+    show_deck(global_args.shuffle_deck);
 
-  return 0;
+    return 0;
 }
